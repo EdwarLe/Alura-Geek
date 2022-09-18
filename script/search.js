f278fb4cff6abb5090b97cae80a3d16587f0d467
Edwar León <edwar.leon.m@gmail.com>
2022-09-18 18:25:47 -0500

    filtrar por busqueda finalizado

--- script/search.js
+++ script/search.js
@@ -1,7 +1,29 @@
-let campoFiltrar = document.getElementById("filtrar__search");
+let campoFiltrar = document.querySelector(".filtrar__search");
 
 campoFiltrar.addEventListener("input", () => {
-    console.log("escribieron en el input");
-    
-    //var objetos = document.querySelectorAll(".container-products__line__products__product__text");
-})
+
+    let productos = document.querySelectorAll(".container-products__line__products__product");
+
+    if (campoFiltrar.value.length > 0) {
+        for (let i = 0; i < productos.length; i++) {
+            let producto = productos[i];
+            let prodNombre = producto.querySelector(".container-products__line__products__product__text");
+            let nombre = prodNombre.textContent;
+            let expresion = RegExp(campoFiltrar.value, "i");
+
+            if (!expresion.test(nombre)) {
+                producto.classList.add("invisible");
+            } else {
+                producto.classList.remove("invisible");
+            }
+
+        }
+    } else {
+        for (let i = 0; i < productos.length; i++) {
+            let producto = productos[i];
+            producto.classList.remove("invisible");
+        }
+
+
+    }
+});
